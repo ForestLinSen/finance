b50a8210465828d3b2beb6e159387e20ca03e7e9
sunnylinsen <sunnylinsen@gmail.com>
2023-04-14 14:33:25 +0100

    Distribution and README updated

--- portfolio.py
+++ portfolio.py
@@ -15,7 +15,7 @@ class Portfolio:
     self.sample_num = sample_num
     self.calculate(line=line)
 
-  # 用雅虎财经API创建Data
+  # 用雅虎财经API创建Data / Create Data with Yahoo Finance API
   def create_stock_data(self, data_symbol):
     data = pd.DataFrame()
     for symbol in data_symbol:
@@ -29,43 +29,51 @@ class Portfolio:
         symbol_data.index = str_index
         data = pd.concat([data, symbol_data['Close']], axis=1)
       except:
-        print("请检查股票代码拼写是否有误！")
+        print("请检查股票代码拼写是否有误! / Please check the spelling of the stock code!")
     data.columns = data_symbol
     data = data.dropna()
     return data
 
   def calculate(self, line):
-    # 初始化参数
-    self.all_weights = np.zeros((self.sample_num, len(self.data.columns)))
-    self.ret_arr = np.zeros(self.sample_num)
-    self.vol_arr = np.zeros(self.sample_num)
-    self.sharpe_arr = np.zeros(self.sample_num)
-    self.log_ret = np.log(self.data/self.data.shift(1))
+      # 初始化参数
+      self.all_weights = np.zeros((self.sample_num, len(self.data.columns)))
+      self.ret_arr = np.zeros(self.sample_num)
+      self.vol_arr = np.zeros(self.sample_num)
+      self.sharpe_arr = np.zeros(self.sample_num)
+      self.log_ret = np.log(self.data/self.data.shift(1))
 
-    for x in range(self.sample_num):
-      # 随机给不同资产赋予权重
-      self.weights = np.array(np.random.random(self.length))
-      self.weights = self.weights/np.sum(self.weights)
-      self.all_weights[x,:] = self.weights
+      for x in range(self.sample_num):
+          # 随机给不同资产赋予权重 / Randomly assign weights to different assets
+          self.weights = np.random.dirichlet(np.ones(self.length), size=1)[0]
+          self.all_weights[x, :] = self.weights
 
-      # 计算期望收益
-      # 252是美股每年的交易天数
-      self.ret_arr[x] = np.sum( (self.log_ret.mean() * self.weights * 252))
-      # 计算期望风险
-      self.vol_arr[x] = np.sqrt(np.dot(self.weights.T, np.dot(self.log_ret.cov()*252, self.weights)))
+          # 计算期望收益 / Calculate expected return
+          # 252是美股每年的交易天数 / 252 is the number of trading days in a year
+          self.ret_arr[x] = np.sum( (self.log_ret.mean() * self.weights * 252))
+          # 计算期望风险 / Calculate expected risk
+          self.vol_arr[x] = np.sqrt(np.dot(self.weights.T, np.dot(self.log_ret.cov()*252, self.weights)))
 
-      # 计算夏普比率
-      self.sharpe_arr[x] = self.ret_arr[x]/ self.vol_arr[x]
+          # 计算夏普比率 / Calculate Sharpe Ratio
+          self.sharpe_arr[x] = self.ret_arr[x]/ self.vol_arr[x]
 
-    self.frontier_x = []
-    self.frontier_y = np.linspace(-0.1, 0.5, 100)
+      self.frontier_x = []
+      self.frontier_y = np.linspace(self.ret_arr.min(), self.ret_arr.max(), 100)
 
-    for possible_return in self.frontier_y:
-      cons = ({'type':'eq','fun': self.check_sum}, {'type':'eq','fun':lambda w : self.get_ret_vol_sr(w)[0]-possible_return})
-      init_guess = np.ones(self.length) / (self.length)
-      bounds = ((0,1),)*(self.length)
-      self.result = minimize(self.minimize_volatility, init_guess, method='SLSQP', bounds=bounds, constraints=cons)
-      self.frontier_x.append(self.result['fun'])
+      for possible_return in self.frontier_y:
+        cons = ({'type':'eq','fun': self.check_sum}, {'type':'eq','fun':lambda w : self.get_ret_vol_sr(w)[0]-possible_return})
+        init_guess = np.ones(self.length) / (self.length)
+        bounds = ((0,1),)*(self.length)
+        self.result = minimize(self.minimize_volatility, init_guess, method='SLSQP', bounds=bounds, constraints=cons)
+        self.frontier_x.append(self.result['fun'])
+
+      # Find the optimal weights with the highest Sharpe ratio on the efficient frontier
+      cons = ({'type': 'eq', 'fun': self.check_sum})
+      bounds = ((0, 1),) * (self.length)
+      init_guess = np.ones(self.length) / self.length
+
+      result_optimal = minimize(self.neg_sharpe, init_guess, method='SLSQP', bounds=bounds, constraints=cons)
+      self.optimal_weights = result_optimal.x
+      self.optimal_ret, self.optimal_vol, _ = self.get_ret_vol_sr(self.optimal_weights)
 
   def draw(self, line=True):
     plt.figure(figsize=(15,8))
@@ -77,16 +85,22 @@ class Portfolio:
     plt.yticks(np.arange(-0.1, 0.6, step=0.2))
     if(line):
       plt.plot(self.frontier_x, self.frontier_y, 'r--', linewidth=3)
+
+    # Plot the red dot on the optimal position on the efficient frontier
+    plt.scatter(self.optimal_vol, self.optimal_ret, c='red', marker='o', s=100, edgecolors='black', label='Optimal Solution')
+    plt.legend()
     plt.show()
+
 
   def report(self):
     self.optim_number = self.sharpe_arr.argmax()
     self.optim_weights = self.all_weights[self.optim_number]
-    print("基于所选风险资产的模拟最佳组合: ")
+    print("所选择的时间范围为：{} - {} / The selected time range is: {} - {}".format(start_date, end_date, start_date, end_date))
+    print("基于所选风险资产的模拟最佳组合: / The best portfolio simulated based on the selected risk assets:")
     for i in range(self.length):
-      print("股票代码：{}, 比例：{:.2f}%".format(self.data.columns[i], self.optim_weights[i]*100))
-    self.best_results_arr = self.get_ret_vol_sr( self.optim_weights)
-    print("期望年化收益: {:.2f}%, 风险: {:.2f}%".format(self.best_results_arr[0]*100, self.best_results_arr[1]*100, self.best_results_arr[2]))
+        print("股票代码(Stock Code): {}, 比例(Proportion)：{:.2f}%".format(self.data.columns[i], self.optim_weights[i]*100))
+    self.best_results_arr = self.get_ret_vol_sr(self.optim_weights)
+    print("期望年化收益(Expected Annual Return): {:.2f}%, 风险(Risk): {:.2f}%".format(self.best_results_arr[0]*100, self.best_results_arr[1]*100, self.best_results_arr[2]))
 
 
   def get_ret_vol_sr(self, weights):
@@ -109,10 +123,10 @@ class Portfolio:
 
 ####根据需要调整下面的参数####
 if __name__ == "__main__":
-    # 数据起始日期
+    # 数据起始日期 / Data start date
     start_date = "2019-01-01"
     end_date = "2022-10-01"
-    # 选择需要哪些资产的数据
+    # 选择需要哪些资产的数据 / Choose which assets to use
     data_array = ["600519.SS", "0700.HK","BABA", "AAPL", "MSFT", "TSLA"]
     # data_array = ["BABA", "AAPL", "MSFT", "TSLA"]
 
